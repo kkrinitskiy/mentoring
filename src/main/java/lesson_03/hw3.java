fa4a14c2d1a662b3ad360161b36e973cc19f2cfc
Kostantin Krinitskiy <kkrinitskiy@internet.ru>
2024-04-10 13:57:23 +0300

    second task done

--- src/main/java/lesson_03/hw3.java
+++ src/main/java/lesson_03/hw3.java
@@ -1,6 +1,5 @@
 package lesson_03;
 
-import java.lang.reflect.Array;
 import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -10,17 +9,46 @@ public class hw3 {
         Scanner scanner = new Scanner(System.in);
 
 
-//                          1 ЗАДАЧА
-
+        System.out.println("1 ЗАДАЧА");
         int[] ints = {10, 20, 30, 40, 50};
 
-        ints = firstTask(scanner, ints);
+//        ints = firstTask(scanner, ints);
 
-        System.out.println(Arrays.toString(ints));
+        System.out.println(Arrays.toString(ints) + "\n");
+
+
+
+        System.out.println("2 ЗАДАЧА");
+        String input =
+                "{" +
+                        "'users' : " +
+                                "{ " +
+                                "'Фамилия' : 'Семёнкин', " +
+                                "'Предмет' : 'Информатика', " +
+                                "'Оценки на уроке' : [5, 4, 5, 5, 3, 4, 5, 5, 5, 5], " +
+                                "'Оценки за контрольные' : [5, 5, 4, 5, 5, 5, 4, 5], " +
+                                "'Преподаватель' : 'Иванова' " +
+                                "}, " +
+                                "{ " +
+                                "'Фамилия' : 'Ивашин', " +
+                                "'Предмет' : 'Математика', " +
+                                "'Оценки на уроке' : [3, 2, 4, 4, 3, 4, 3, 5, 4, 4], " +
+                                "'Оценки за контрольные' : [3, 3, 4, 5, 4, 4, 3, 5], " +
+                                "'Преподаватель' : 'Иванова' " +
+                                "} " +
+                "}";
+
+        System.out.println(secondTask(input) + "\n");
+
+
+
+        System.out.println("3 ЗАДАЧА");
 
 
 
     }
+
+
 
     public static int[] firstTask(Scanner scanner, int[] ints){
         System.out.println("введите число n");
@@ -53,5 +81,83 @@ public class hw3 {
 
     }
 
+
+    private static String secondTask(String input){
+        StringBuilder result = new StringBuilder();
+
+        String[] users = input.split("}, \\{ ");
+
+        for (String user : users) {
+           result.append(getData(user)).append("\n");
+        }
+
+
+        return result.toString().strip();
+    }
+
+
+    private static String getDiscipline(String input) {
+        String str = "'Предмет' : '";
+
+        int start = input.lastIndexOf(str) + str.length();
+
+        int end = input.indexOf("'", start);
+
+        return input.substring(start, end);
+    }
+
+    private static String getSurname(String input) {
+        String str = "'Фамилия' : '";
+
+        int start = input.lastIndexOf(str) + str.length();
+
+        int end = input.indexOf("'", start);
+
+        return input.substring(start, end);
+    }
+
+    private static double getAverageOnClassWork(String input) {
+        String str = "'Оценки на уроке' : [";
+
+        int start = input.lastIndexOf(str) + str.length();
+
+        int end = input.indexOf("]", start);
+
+        calculateAverage(input.substring(start, end));
+
+        return calculateAverage(input.substring(start, end));
+    }
+
+    private static double getAverageOnTest(String input) {
+        String str = "'Оценки за контрольные' : [";
+
+        int start = input.lastIndexOf(str) + str.length();
+
+        int end = input.indexOf("]", start);
+
+        return calculateAverage(input.substring(start, end));
+    }
+
+    private static double calculateAverage(String grades){
+        double sum = 0;
+        int count = 0;
+        String gradesInLine = grades.replaceAll("\\D", "");
+        char[] chars = gradesInLine.toCharArray();
+
+        for (char aChar : chars) {
+            sum += Character.getNumericValue(aChar);
+            count++;
+        }
+
+        return sum/count;
+    }
+
+    private static String getData(String user){
+
+        return getDiscipline(user) + " " +
+                getSurname(user) + " " +
+                getAverageOnClassWork(user) + " " +
+                getAverageOnTest(user);
+    }
 
 }
