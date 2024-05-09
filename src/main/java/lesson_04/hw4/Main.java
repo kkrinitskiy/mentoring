606f545582f485551348ecc396d7479b03ea7cbd
Konstantin Krinitskiy <kkrinitskiy@internet.ru>
2024-05-09 09:09:59 +0300

    enum with taxes added

--- src/main/java/lesson_04/hw4/Main.java
+++ src/main/java/lesson_04/hw4/Main.java
@@ -7,40 +7,40 @@ import lesson_04.hw4.item_models.Car;
 import lesson_04.hw4.item_models.Elephant;
 import lesson_04.hw4.item_models.Panzer;
 import lesson_04.hw4.transport_company_core.Container;
+import lesson_04.hw4.transport_company_core.Tax;
 import lesson_04.hw4.transport_company_core.TransportCompany;
 import lesson_04.hw4.transport_company_core.Transportable;
 
 import java.text.MessageFormat;
 import java.util.*;
 
+import static lesson_04.hw4.transport_company_core.Tax.*;
+
 public class Main {
-    public static final double CAR_TAX = 2.4;
-    public static final double ANIMAL_TAX = 3.6;
-    public static final double WEAPON_TAX = 2.9;
     public static void main(String[] args) {
 
         TransportCompany yandexDostavka = new TransportCompany(10);
 
-        List<Transportable> items = new ArrayList<>();
+        List<Transportable> items = new ArrayList<>(); 
 
-        Car car_00 = new Car(HWUtils.getRandomWidth(), HWUtils.getRandomHight(), HWUtils.getRandomLength(), CAR_TAX);
-        Car car_01 = new Car(HWUtils.getRandomWidth(), HWUtils.getRandomHight(), HWUtils.getRandomLength(), CAR_TAX);
-        Car car_02 = new Car(HWUtils.getRandomWidth(), HWUtils.getRandomHight(), HWUtils.getRandomLength(), CAR_TAX);
+        Car car_00 = new Car(HWUtils.getRandomWidth(), HWUtils.getRandomHight(), HWUtils.getRandomLength(), CAR.getRate());
+        Car car_01 = new Car(HWUtils.getRandomWidth(), HWUtils.getRandomHight(), HWUtils.getRandomLength(), CAR.getRate());
+        Car car_02 = new Car(HWUtils.getRandomWidth(), HWUtils.getRandomHight(), HWUtils.getRandomLength(), CAR.getRate());
         items.add(car_00);
         items.add(car_01);
         items.add(car_02);
       
-        Elephant elephant_00 = new Elephant(HWUtils.getRandomWidth(), HWUtils.getRandomHight(), HWUtils.getRandomLength(), ANIMAL_TAX);
-        Elephant elephant_01 = new Elephant(HWUtils.getRandomWidth(), HWUtils.getRandomHight(), HWUtils.getRandomLength(), ANIMAL_TAX);
-        Elephant elephant_02 = new Elephant(HWUtils.getRandomWidth(), HWUtils.getRandomHight(), HWUtils.getRandomLength(), ANIMAL_TAX);
+        Elephant elephant_00 = new Elephant(HWUtils.getRandomWidth(), HWUtils.getRandomHight(), HWUtils.getRandomLength(), ANIMAL.getRate());
+        Elephant elephant_01 = new Elephant(HWUtils.getRandomWidth(), HWUtils.getRandomHight(), HWUtils.getRandomLength(), ANIMAL.getRate());
+        Elephant elephant_02 = new Elephant(HWUtils.getRandomWidth(), HWUtils.getRandomHight(), HWUtils.getRandomLength(), ANIMAL.getRate());
         items.add(elephant_00);
         items.add(elephant_01);
         items.add(elephant_02);
 
-        Panzer panzer_00 = new Panzer(HWUtils.getRandomWidth(), HWUtils.getRandomHight(), HWUtils.getRandomLength(), WEAPON_TAX);
-        Panzer panzer_01 = new Panzer(HWUtils.getRandomWidth(), HWUtils.getRandomHight(), HWUtils.getRandomLength(), WEAPON_TAX);
-        Panzer panzer_02 = new Panzer(HWUtils.getRandomWidth(), HWUtils.getRandomHight(), HWUtils.getRandomLength(), WEAPON_TAX);
-        Panzer panzer_03 = new Panzer(600, HWUtils.getRandomHight(), HWUtils.getRandomLength(), WEAPON_TAX);
+        Panzer panzer_00 = new Panzer(HWUtils.getRandomWidth(), HWUtils.getRandomHight(), HWUtils.getRandomLength(), WEAPON.getRate());
+        Panzer panzer_01 = new Panzer(HWUtils.getRandomWidth(), HWUtils.getRandomHight(), HWUtils.getRandomLength(), WEAPON.getRate());
+        Panzer panzer_02 = new Panzer(HWUtils.getRandomWidth(), HWUtils.getRandomHight(), HWUtils.getRandomLength(), WEAPON.getRate());
+        Panzer panzer_03 = new Panzer(600, HWUtils.getRandomHight(), HWUtils.getRandomLength(), WEAPON.getRate());
         items.add(panzer_00);
         items.add(panzer_01);
         items.add(panzer_02);
@@ -50,7 +50,7 @@ public class Main {
 
         for (Transportable item : items) {
             yandexDostavka.loadingItem(item);
-        }
+        }   
 
 
     }
@@ -73,7 +73,7 @@ public class Main {
                 item.getLength(),
                 item.costDelivery()));
 
-        System.out.println("\nОсатльные:");
+        System.out.println("\nВсе:");
         list.forEach(i -> System.out.println(
             MessageFormat.format(
                 "{0}, с габаритами: {1}x{2}x{3}, доставка стоит: {4}", 
